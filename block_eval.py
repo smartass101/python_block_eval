42b5c5b19649f16866a6289c2dc7d9d5d74d415d
Ondrej Grover <ondrej.grover@gmail.com>
2015-12-05 15:32:10 +0100

    use exec for the exec part as having exec in a function is supposed to prevent locals optimization
    as per http://stackoverflow.com/a/8028785
    However, this may not work in Python3
    maybe the ctypes call should be executed before eval

--- block_eval.py
+++ block_eval.py
@@ -39,7 +39,7 @@ def block_eval(code_str, globals_=None, locals_=None, block_name='<string>'):
     if isinstance(last_stmt, ast.Expr):
         del code_ast.body[-1]
         if len(code_ast.body) > 0:
-            eval(compile(code_ast, block_name, 'exec'), p_globals, p_locals)
+            exec(compile(code_ast, block_name, 'exec'), p_globals, p_locals)
             locals()            # update local symbol table after eval
         expr = ast.Expression(last_stmt.value)
         final_code = compile(expr, block_name, 'eval')
